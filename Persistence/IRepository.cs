c33a5fb82a3241cc7822cca290a5d3f2fc966ebb
Chandan Rughooputh <chandan@structureit-international.net>
2017-05-31 12:50:44 +0400

    Add inital files

---
+++ Persistence/IRepository.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Collections.Generic;
+
+namespace Persistence
+{
+    public interface IRepository
+    {
+        IList<T> RunQuery<T>(string query);
+    }
+}
---
+++ Persistence/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Persistence
+{
+    public class Repository : IRepository
+    {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings["bookstore"].ConnectionString;
+
+        public IList<T> RunQuery<T>(string query)
+        {
+
+            var results = new List<T>();
+
+            using (var connection = new SqlConnection(connString))
+            {
+                connection.Open();
+                using (var sqlCommand = new SqlCommand())
+                {
+                    sqlCommand.CommandText = query;
+                    sqlCommand.Connection = connection;
+
+                    using (var reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var item = Activator.CreateInstance<T>();
+                            foreach (var property in typeof(T).GetProperties())
+                            {
+                                if (!reader.IsDBNull(reader.GetOrdinal(property.Name)))
+                                {
+                                    var convertTo = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                                    property.SetValue(item, Convert.ChangeType(reader[property.Name], convertTo), null);
+                                }
+                            }
+                            results.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+    } 
+}
